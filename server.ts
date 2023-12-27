89cd28cb5673f834254f20fe3d5c5b537324ba54
Khizri Makhmudov <khizrim@khizrim.ru>
2023-12-27 16:27:43 +0300

    feat: uses port from environment if defined or uses 3000

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import express, { Express } from 'express';
 
 const app: Express = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static('./dist'));
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
