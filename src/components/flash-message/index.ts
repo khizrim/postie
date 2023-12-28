4d5abc00138bd19cc7734f1ca14bcc1306a95802
Khizri Makhmudov <khizrim@khizrim.ru>
2023-12-28 21:12:53 +0300

    feat: adds flash message component

---
+++ src/components/flash-message/index.ts
@@ -0,0 +1,1 @@
+export { default as FlashMessage } from './flash-message.hbs?raw';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button';
+export * from './flash-message';
 export * from './form';
 export * from './input';
 export * from './link';
