d6a6b4ef46d3545e992c594361fd7a5c04d8cb81
Khizri Makhmudov <kh.makhmudov@me.com>
2024-03-01 12:17:31 +0300

    feat: add basic Block and EventBus implementations

---
+++ src/core/event-bus/event-bus.ts
@@ -0,0 +1,35 @@
+import type { Listener, Listeners } from './event-bus.types';
+
+export class EventBus {
+  protected listeners: Listeners;
+
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event: string, callback: Listener): void {
+    if (!(event in this.listeners)) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: Listener): void {
+    if (!(event in this.listeners)) {
+      throw new Error(`Missing event: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event].filter((listener) => listener !== callback);
+  }
+
+  emit(event: string, ...args: unknown[]): void {
+    if (!(event in this.listeners)) {
+      throw new Error(`Missing event: ${event}`);
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
