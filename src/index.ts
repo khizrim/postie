0f12dee48d42e22399c1f46c8ad6c0dc93a63deb
Khizri Makhmudov <kh.makhmudov@me.com>
2024-05-15 23:02:37 +0300

    feat: add submit events to sign-up and sign-in, register custom Handlebars helpers

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,20 @@ import Handlebars from 'handlebars';
 
 import { navigate } from 'src/core/navigate';
 import { registerComponent } from 'src/core/register-component';
-import 'src/helpers/get-value-by-key';
-import 'src/helpers/is-current-chat';
+import { registerCustomHelpers } from 'src/core/register-custom-helpers';
+import { getValueByKey } from 'src/helpers/get-value-by-key';
+import { isCurrentChat } from 'src/helpers/is-current-chat';
 
 import * as Components from './components';
 import * as Layouts from './layouts';
 import * as Partials from './partials';
 
 import './index.css';
+
+export const customHelpers = {
+  isCurrentChat,
+  getValueByKey,
+};
 
 Object.entries({ ...Partials, ...Layouts }).forEach(([name, component]) => {
   Handlebars.registerPartial(name, component as Template);
@@ -21,5 +27,6 @@ Object.entries({ ...Components }).forEach(([name, component]) => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
+  registerCustomHelpers(customHelpers);
   navigate('sign-in');
 });
