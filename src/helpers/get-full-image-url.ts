d576271c3480f1861d9baee38f9e1e55a1ce5169
Khizri Makhmudov <k.makhmudov@tinkoff.ru>
2024-06-20 16:04:30 +0300

    fix: handle default avatar and image URLs correctly
    - Use getDefaultAvatar helper for user avatar in settings (settings.hbs)
    - Update getFullImageUrl to support data URLs (get-full-image-url.ts)

--- src/helpers/get-full-image-url.ts
+++ src/helpers/get-full-image-url.ts
@@ -1,6 +1,10 @@
 import { API_URL } from 'src/utils/constants.ts';
 
-export const getFullImageUrl = (path: string): string =>
-  path.startsWith('http') || path.startsWith('/src/assets') || path.startsWith('data:image')
+export const getFullImageUrl = (path: string): string => {
+  return path.startsWith('http') ||
+    path.startsWith('/src/assets') ||
+    path.startsWith('/assets') ||
+    path.startsWith('data:image')
     ? path
     : `${API_URL}/resources${path}`;
+};
